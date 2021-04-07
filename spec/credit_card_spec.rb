15484bc6e9dbb61a6f975302ef0548f40bf48a63
Adam Phillips <adam2000phillips@gmail.com>
2021-04-07 15:57:32 +0100

    Second Test - Client is able to deposit which should update the balance amount according to the deposited ammount

--- spec/credit_card_spec.rb
+++ spec/credit_card_spec.rb
@@ -1,9 +1,16 @@
 require 'credit_card'
 describe CreditCard do
+  test_value = 50
   before(:each) do
      @credit_card = CreditCard.new
    end
-  it "has a default balance of 0 when initialized" do
-    expect(@credit_card.balance).to eq(0)
+  describe '#deposit' do
+    it "has a default balance of 0 when initialized" do
+      expect(@credit_card.balance).to eq(0)
+    end
+    it "deposits the deposit value as the argument" do
+      @credit_card.deposit(test_value)
+      expect(@credit_card.balance).to eq test_value
+    end
   end
 end
