d2a849380ce8dd7a0bdf942f3c5e73f2ed29fecb
Adam Phillips <adam2000phillips@gmail.com>
2021-04-07 15:40:31 +0100

    First test, each card starts with a default balance of 0 when nothing is deposited

--- spec/credit_card_spec.rb
+++ spec/credit_card_spec.rb
@@ -1,4 +1,9 @@
 require 'credit_card'
 describe CreditCard do
-
+  before(:each) do
+     @credit_card = CreditCard.new
+   end
+  it "has a default balance of 0 when initialized" do
+    expect(@credit_card.balance).to eq(0)
+  end
 end
