c222536ca201da4a60268f4450e2eb1b5f2e2798
Adam Phillips <adam2000phillips@gmail.com>
2021-04-08 03:02:55 +0100

    Adding interactivity to choose the date of the deposit/withdrawal

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -4,10 +4,17 @@ attr_reader :balance
   def initialize(balance = DEFAULTBALANCE)
       @balance = balance
   end
+
   def deposit(deposit_value)
+    puts "Enter the date of this deposit"
+    date_deposit = gets.chomp
     @balance = @balance + deposit_value
   end
+
   def withdraw(withdraw_value)
+    puts "Enter the date of this withdawal"
+    date_withdrawal = gets.chomp
     @balance = @balance - withdraw_value
   end
+
 end
